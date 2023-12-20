0aae1c23bb5655f94d96e7702c13c2c5bf448b83
Tomasz <tomasz.iwanicki@onet.pl>
2023-12-20 08:28:42 +0100

    Create a subtask component

---
+++ app/components/Subtask.jsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { useState } from 'react';
+
+const Subtask = ({ isCompleted, title }) => {
+  const [completed, setCompleted] = useState(isCompleted);
+
+  return (
+    <div
+      className="p-3 flex items-center gap-4 bg-veryDarkGray hover:cursor-pointer"
+      onClick={() => setCompleted((prev) => !prev)}
+    >
+      <input
+        onChange={() => {}}
+        type="checkbox"
+        checked={completed}
+        // defaultChecked={completed ? true : false}
+      />
+      <h4 className={`${completed ? 'text-mediumGray line-through' : 'text-white hover:text-neutral-400 font-bold'}`}>{title}</h4>
+    </div>
+  );
+};
+
+export default Subtask;
