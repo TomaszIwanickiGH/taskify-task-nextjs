df3808cf8167b8ba5cc9a6c98e3486c329bae340
Tomasz <tomasz.iwanicki@onet.pl>
2023-12-20 08:29:55 +0100

    Create a custom, reusable input component

---
+++ app/components/CustomInput.jsx
@@ -0,0 +1,42 @@
+'use client';
+
+import Image from 'next/image';
+import { images } from '../constants';
+
+const CustomInput = ({ label, placeholder, textarea, deleteIcon, value, setValue, handleDelete }) => {
+  return (
+    <div className="flex justify-center items-center gap-4 w-full text-white">
+      <label className="flex flex-1 flex-col gap-2">
+        {label && <h3 className="font-semibold text-[15px]">{label}</h3>}
+        {textarea ? (
+          <textarea
+            value={value}
+            onChange={setValue}
+            rows={3}
+            className="py-2 px-3 border-[1px] rounded-lg border-neutral-600 bg-darkGray outline-none focus:outline-none hover:cursor-pointer resize-none placeholder:text-[15px]"
+            placeholder={placeholder}
+          />
+        ) : (
+          <input
+            type="text"
+            value={value}
+            onChange={setValue}
+            className="py-2 px-3 border-[1px] rounded-lg border-neutral-600 bg-darkGray outline-none focus:outline-none hover:cursor-pointer placeholder:text-mediumGray placeholder:text-[15px]"
+            placeholder={placeholder}
+          />
+        )}
+      </label>
+      {deleteIcon && (
+        <div onClick={handleDelete}>
+          <Image
+            src={images.iconClose}
+            alt="delete"
+            className="w-[15px] h-[15px] hover:cursor-pointer"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CustomInput;
