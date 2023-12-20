b051bf0aae08d5fedf817efbbc9a5379c9b65353
Tomasz <tomasz.iwanicki@onet.pl>
2023-12-20 08:16:59 +0100

    Create an API route for adding a board

---
+++ app/api/board/new/route.js
@@ -0,0 +1,18 @@
+import connectToDB from '@/app/utils/database';
+import Board from '@/app/models/board';
+
+export const POST = async (req, res) => {
+  const { name, columns } = await req.json();
+  console.log(name, columns);
+  try {
+    await connectToDB();
+
+    const newBoard = new Board({ name, columns });
+
+    await newBoard.save();
+
+    return new Response(JSON.stringify(newBoard), { status: 200 });
+  } catch (error) {
+    return new Response('Failed to create a board', { status: 500 });
+  }
+};
