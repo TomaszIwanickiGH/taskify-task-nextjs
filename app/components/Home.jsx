3e72ac6d5c6038e2e1daaf56da587dab7bfe64cd
Tomasz <tomasz.iwanicki@onet.pl>
2023-12-27 19:22:05 +0100

    Display 'Add Column' when there are no columns

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -4,12 +4,14 @@ import { useEffect } from 'react';
 import TasksContainer from './TasksContainer';
 
 import useGlobals from '../hooks/useGlobals';
+import useEditBoardModal from '../hooks/board/useEditBoardModal';
 
 const Home = () => {
   const globals = useGlobals();
 
   const sidebar = globals.isSidebarOpen;
   const currentBoard = globals.currentBoard;
+  const editBoardModal = useEditBoardModal();
 
   //Fetch boards from database
   useEffect(() => {
@@ -26,20 +28,26 @@ const Home = () => {
   const fetchedBoards = globals.fetchedBoards;
 
   const board = fetchedBoards.boards.filter((board) => board.name === currentBoard);
-  const columns = board.map((item) => item.columns);
+  const cols = board.map((item) => item.columns);
+  const columns = cols[0];
 
   return (
     <div className={`bg-veryDarkGray p-8 flex sm:flex-row flex-col sm:items-start items-center sm:gap-6 gap-16 h-full overflow-x-auto translate duration-300 ${sidebar ? 'lg:max-w-[80vw] max-w-full' : 'max-w-full'}`}>
-      {columns.map((column) =>
-        column.map((item, index) => {
-          return (
-            <TasksContainer
-              key={index}
-              stage={item.name}
-              tasks={item.tasks}
-            />
-          );
-        })
+      {columns && columns.length !== 0 ? (
+        columns.map((column, index) => (
+          <TasksContainer
+            key={index}
+            stage={column.name}
+            tasks={column.tasks}
+          />
+        ))
+      ) : (
+        <div
+          onClick={() => editBoardModal.onOpen()}
+          className="h-[80vh] w-full bg-[#2B2C37] rounded-lg flex justify-center items-center px-8 translate duration-300 hover:cursor-pointer hover:bg-[#2B2C37]/50"
+        >
+          <p className="text-[24px] text-mediumGray font-semibold">+ New Column</p>
+        </div>
       )}
     </div>
   );
