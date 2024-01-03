682dd6ae7c776d3e066df88e2c5a1f0da98a35d6
Tomasz <tomasz.iwanicki@onet.pl>
2024-01-03 06:03:29 +0100

    Add disabled option

--- app/components/CustomInput.jsx
+++ app/components/CustomInput.jsx
@@ -3,7 +3,7 @@
 import Image from 'next/image';
 import { images } from '../constants';
 
-const CustomInput = ({ label, placeholder, textarea, deleteIcon, value, setValue, handleDelete }) => {
+const CustomInput = ({ label, placeholder, textarea, deleteIcon, value, setValue, handleDelete, disabled }) => {
   return (
     <div className="flex justify-center items-center gap-4 w-full text-white">
       <label className="flex flex-1 flex-col gap-2">
@@ -18,10 +18,11 @@ const CustomInput = ({ label, placeholder, textarea, deleteIcon, value, setValue
           />
         ) : (
           <input
+            disabled={disabled}
             type="text"
             value={value}
             onChange={setValue}
-            className="py-2 px-3 border-[1px] rounded-lg border-neutral-600 bg-darkGray outline-none focus:outline-none hover:cursor-pointer placeholder:text-mediumGray placeholder:text-[15px]"
+            className={`py-2 px-3 border-[1px] rounded-lg border-neutral-600 bg-darkGray outline-none focus:outline-none placeholder:text-mediumGray placeholder:text-[15px] ${disabled ? '' : 'hover:cursor-pointer'}`}
             placeholder={placeholder}
           />
         )}
